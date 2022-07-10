38bbfcba3e785b5129d755d9951ca900e441a4cc
Adriana Morales <adriana.morales@internal.salesforce.com>
2022-07-10 14:41:45 -0700

    working on login page

--- ui/src/Components/Login/Login.jsx
+++ ui/src/Components/Login/Login.jsx
@@ -1,8 +1,45 @@
 import * as React from "react";
 import "./Login.css";
+import Stack from "@mui/material/Stack";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 
 const Login = () => {
-  return "Login Page";
+  return (
+    <div className="container">
+      <Box>
+        <Stack
+          component="form"
+          sx={{
+            // width: "25ch",
+            width: 500,
+            maxWidth: "100%",
+          }}
+          spacing={2}
+          noValidate
+          autoComplete="off"
+        >
+          <div className="header">
+            <h2>Login</h2>
+          </div>
+          <br></br>
+          <label classfor="email">Email</label>
+          <TextField fullWidth label="user@gmail.com" id="email" />
+          <label for="password">Password</label>
+          <TextField fullWidth label="password" id="password" />
+          <Button variant="contained">Login</Button>
+
+          <div className="footer">
+            <p>
+              Don't have an account? Sign up
+              <a href="/Register"> here.</a>
+            </p>
+          </div>
+        </Stack>
+      </Box>
+    </div>
+  );
 };
 
 export default Login;
