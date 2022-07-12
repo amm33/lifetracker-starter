f437c210c89a22dd17874eea9c6536220a18c5b4
Adriana Morales <adriana.morales@internal.salesforce.com>
2022-07-12 11:08:08 -0700

    working on the register page

--- ui/src/Components/Login/Login.jsx
+++ ui/src/Components/Login/Login.jsx
@@ -1,11 +1,52 @@
 import * as React from "react";
 import "./Login.css";
+import { useState } from "react";
 import Stack from "@mui/material/Stack";
 import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 
-const Login = () => {
+const Login = (props) => {
+  //form of the login page
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  //handling login inputs
+  const handleOnInputChange = (event) => {
+    //email portion
+    if (event.target.name === "email") {
+      if (event.target.value.indexOf("@") === -1) {
+        setErrors((e) => ({ ...e, email: "Please enter a valid email." }));
+      } else {
+        setErrors((e) => ({ ...e, email: null }));
+      }
+    }
+
+    //password portion
+    if (event.target.name == "password") {
+      if (event.target.value.length < 8) {
+        setErrors((e) => ({
+          ...e,
+          password: "Please enter valid password with greater length than 8",
+        }));
+      } else {
+        setErrors((e) => ({ ...e, password: null }));
+      }
+    }
+
+    setForm((f) => ({ ...f, [event.target.name]: event.target.value }));
+  };
+
+  //handling on submit
+  const handleOnSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    setErrors((e) => ({ ...e, form: null }));
+  };
+
+  //body of the page
   return (
     <div className="container">
       <Box>
