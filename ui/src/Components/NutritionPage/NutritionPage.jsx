ec7027a193379a072e4a3368c9cb1a9be1a2f46a
Adriana Morales <adriana.morales@internal.salesforce.com>
2022-07-14 16:19:13 -0700

    Deploying express api

--- ui/src/Components/NutritionPage/NutritionPage.jsx
+++ ui/src/Components/NutritionPage/NutritionPage.jsx
@@ -1,8 +1,14 @@
 import * as React from "react";
-import LoginForm from "../LoginForm/LoginForm";
 import "./NutritionPage.css";
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import NutritionOverview from "../NutritionOverview/NutritionOverview";
+import NutritionNew from "../NutritionNew/NutritionNew";
+import NutritionDetail from "../NutritionDetail/NutritionDetail";
+import LoginForm from "../LoginForm/LoginForm";
+import apiClient from "../../services/apiClient";
 
-const NutritionPage = (props) => {
+export default function NutritionPage(props) {
   return (
     <div className="nutrition-page">
       {props.user.email ? (
@@ -10,12 +16,36 @@ const NutritionPage = (props) => {
           <div className="banner">
             <h1>Nutrition</h1>
           </div>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <NutritionOverview
+                  nutrition={props.nutrition}
+                  user={props.user}
+                ></NutritionOverview>
+              }
+            ></Route>
+            <Route
+              path="/create"
+              element={
+                <NutritionNew
+                  handleNutritonForm={props.handleNutritonForm}
+                  addNutrition={props.addNutrition}
+                ></NutritionNew>
+              }
+            ></Route>
+            <Route
+              path="/id/:nutritionId"
+              element={
+                <NutritionDetail nutrition={props.nutrition}></NutritionDetail>
+              }
+            ></Route>
+          </Routes>
         </main>
       ) : (
         <LoginForm user={props.user} setUser={props.setUser}></LoginForm>
       )}
     </div>
   );
-};
-
-export default NutritionPage;
+}
